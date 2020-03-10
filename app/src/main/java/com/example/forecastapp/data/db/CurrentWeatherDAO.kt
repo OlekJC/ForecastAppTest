419b35445ad514136018f38017444a5072eee503
Aleksander Jacukowicz <a.jacukowicz@samsung.local>
2020-03-10 14:59:56 +0100

    Fixed DAO and DB constant

--- app/src/main/java/com/example/forecastapp/data/db/CurrentWeatherDAO.kt
+++ app/src/main/java/com/example/forecastapp/data/db/CurrentWeatherDAO.kt
@@ -1,16 +1,19 @@
 package com.example.forecastapp.data.db
 
+import androidx.lifecycle.LiveData
 import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
+import androidx.room.Query
 import com.example.forecastapp.data.db.entity.CurrentWeatherResponse
+import com.example.forecastapp.data.db.entity.TODAY_INDEX
 
 @Dao
 interface CurrentWeatherDAO {
 
-    //@Insert(onConflict = OnConflictStrategy.REPLACE)
-    //fun upsert(weatherEntry: CurrentWeatherResponse)
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun upsert(weatherEntry: CurrentWeatherResponse)
 
-    //@Query("SELECT * FROM current_weather where id = $CURRENT_WEATHER_ID")
-    //fun getWeather() : LiveData<CurrentWeatherResponse>
+    @Query("SELECT * FROM current_weather where id = $TODAY_INDEX")
+    fun getWeather() : LiveData<CurrentWeatherResponse>
 }
