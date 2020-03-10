7c72b69804bc5b7b07f365aa7df2bc0bff185a2d
Aleksander Jacukowicz <a.jacukowicz@samsung.local>
2020-03-10 14:14:46 +0100

    Working DB with Retrofit2

--- app/src/main/java/com/example/forecastapp/data/db/CurrentWeatherDAO.kt
+++ app/src/main/java/com/example/forecastapp/data/db/CurrentWeatherDAO.kt
@@ -1,19 +1,16 @@
 package com.example.forecastapp.data.db
 
-import androidx.lifecycle.LiveData
 import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
-import androidx.room.Query
-import com.example.forecastapp.data.db.entity.CURRENT_WEATHER_ID
 import com.example.forecastapp.data.db.entity.CurrentWeatherResponse
 
 @Dao
 interface CurrentWeatherDAO {
 
-    @Insert(onConflict = OnConflictStrategy.REPLACE)
-    fun upsert(weatherEntry: CurrentWeatherResponse)
+    //@Insert(onConflict = OnConflictStrategy.REPLACE)
+    //fun upsert(weatherEntry: CurrentWeatherResponse)
 
-    @Query("SELECT * FROM current_weather where id = $CURRENT_WEATHER_ID")
-    fun getWeather() : LiveData<CurrentWeatherResponse>
+    //@Query("SELECT * FROM current_weather where id = $CURRENT_WEATHER_ID")
+    //fun getWeather() : LiveData<CurrentWeatherResponse>
 }
--- app/src/main/java/com/example/forecastapp/data/db/entity/CurrentWeatherResponse.kt
+++ app/src/main/java/com/example/forecastapp/data/db/entity/CurrentWeatherResponse.kt
@@ -1,11 +1,19 @@
 package com.example.forecastapp.data.db.entity
 
 
+import androidx.room.Embedded
+import androidx.room.Entity
+import androidx.room.PrimaryKey
 import com.google.gson.annotations.SerializedName
 
+@Entity(tableName = "current_weather")
 data class CurrentWeatherResponse(
     @SerializedName("DailyForecasts")
     val dailyForecasts: List<DailyForecast>,
+    @Embedded(prefix = "headline_")
     @SerializedName("Headline")
     val headline: Headline
-)
+) {
+    @PrimaryKey(autoGenerate = false)
+    var id : Int = 0
+}
