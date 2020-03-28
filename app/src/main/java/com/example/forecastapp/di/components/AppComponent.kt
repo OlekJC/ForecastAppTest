46a51862567a8620269c8d9b7d7732478783bce1
Aleksander Jacukowicz <aleksander.jacukowicz@gmail.com>
2020-03-28 22:11:25 +0100

    Split ViewModule module to ViewModel and Repository module

--- app/src/main/java/com/example/forecastapp/di/components/AppComponent.kt
+++ app/src/main/java/com/example/forecastapp/di/components/AppComponent.kt
@@ -1,13 +1,14 @@
 package com.example.forecastapp.di.components
 
 import com.example.forecastapp.di.modules.NetworkModule
+import com.example.forecastapp.di.modules.RepositoryModule
 import com.example.forecastapp.di.modules.ViewModelModule
 import com.example.forecastapp.ui.weather.current.CurrentWeatherFragment
 import dagger.Component
 import javax.inject.Singleton
 
 @Singleton
-@Component(modules = [NetworkModule::class, ViewModelModule::class])
+@Component(modules = [NetworkModule::class, ViewModelModule::class,RepositoryModule::class])
 public interface AppComponent {
     fun inject(currentWeatherFragment: CurrentWeatherFragment)
 }
